b83d2bfe89678f0083b2e4fa29baeb9f229741af
Mufti Bin Habib <muftibinhabib500@gmail.com>
2025-04-18 09:42:13 +0600

    Add files via upload
    used useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
 
 const App = () => {
   let [count , setNumber] = useState(0)
+  const [multiplesOfFive, setMultiplesOfFive] = useState(0);
 
 
 
@@ -11,6 +12,13 @@ const App = () => {
     }, 1000)
   }
   
+  useEffect(() => {
+    if (count !== 0 && count % 5 === 0) {
+      
+      setMultiplesOfFive(prev => prev + 1);
+    }
+  }, [count]);
+  
 
 
   return (
@@ -18,7 +26,7 @@ const App = () => {
   <h1 className='text-center bg-blue-500 text-white text-2xl pb-2'>Timer</h1>
     <h2 className='text-center'>{count}</h2>
     <button className=' relative left-1/2 -translate-x-1/2 mt-5 outline ps-5 pe-5 pt-2 pb-2' onClick={handleIncrement}>Start</button>
-    
+    <h2>Multiples of 5: <span>{multiplesOfFive}</span></h2>
     </>
   )
 }
